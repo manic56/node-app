211c9262ee86dfdaa1339d5c68249f0b2d97edbe
manic56 <manikandan@pearlthoughts.com>
2023-11-15 21:59:26 +0530

    routes added

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  if (req.url === '/v5/greetings'){
+  if(req.url === "/greetings"){
+    res.end("greeting")
+  }
+  else if(req.url === "/v5/greetings"){
+    res.end("v5 greeting")
+  }
+  else {
     const msg = 'Hello world!\n'
     res.end(msg); 
   }
-  
-});
+
+  });
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
